211a6c6ad774ce03debd956cbd0a0dae374b98b3
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-25 13:03:07 +0800

    revert(dbrunner): Drop cache query
    We need this for the store of query.
    
    This reverts commit c96b4930986618e9c595b1d94f644e6ed7fb726b.

--- internal/dbrunner/structure.go
+++ internal/dbrunner/structure.go
@@ -5,6 +5,19 @@ type Input struct {
 	Query string `json:"query"`
 }
 
+func (i Input) Normalize() (Input, error) {
+	// Init should not be normalized and usually constrant.
+	normlizedQuery, err := FormatSQL(i.Query)
+	if err != nil {
+		return Input{}, err
+	}
+
+	return Input{
+		Init:  i.Init,
+		Query: normlizedQuery,
+	}, nil
+}
+
 type Output struct {
 	Result [][]struct {
 		Column string
