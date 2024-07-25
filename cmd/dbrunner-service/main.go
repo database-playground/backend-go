d4b1872a102b82a6d1faff8b7b7d0fa8671fdc0f
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-26 01:45:31 +0800

    feat(dbrunner/service): Require Connect-Protocol-Version to improve security

--- cmd/dbrunner-service/main.go
+++ cmd/dbrunner-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"connectrpc.com/connect"
 	"github.com/database-playground/backend/gen/dbrunner/v1/dbrunnerv1connect"
 	httpservermodule "github.com/database-playground/backend/internal/modules/httpserver"
 	redismodule "github.com/database-playground/backend/internal/modules/redis"
@@ -11,6 +12,6 @@ import (
 
 func main() {
 	fx.New(slogmodule.FxOptions, redismodule.FxModule, dbrunnerservice.FxModule, fx.Provide(func(s *dbrunnerservice.Service) httpservermodule.HTTPHandler {
-		return httpservermodule.WrapHTTPHandler[dbrunnerv1connect.DbRunnerServiceHandler](dbrunnerv1connect.NewDbRunnerServiceHandler, s)
+		return httpservermodule.WrapHTTPHandler[dbrunnerv1connect.DbRunnerServiceHandler](dbrunnerv1connect.NewDbRunnerServiceHandler, s, connect.WithRequireConnectProtocolHeader())
 	}), httpservermodule.FxModule).Run()
 }
