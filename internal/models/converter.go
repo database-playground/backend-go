e35b90950dd688fe425eac3d14152a6172a52a6b
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-29 01:05:15 +0800

    feat(models): Add Question converter

--- internal/models/converter.go
+++ internal/models/converter.go
@@ -6,11 +6,13 @@ import (
 	"time"
 
 	questionmanagerv1 "github.com/database-playground/backend/gen/questionmanager/v1"
+	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // goverter:converter
 // goverter:extend TimeToTimestamp
+// goverter:extend UUIDToString
 type Converter interface {
 	// goverter:ignore state sizeCache unknownFields
 	// goverter:map ID Id
@@ -20,8 +22,24 @@ type Converter interface {
 	// goverter:map ID Id
 	// goverter:map InitialSQL InitialSql
 	SchemaInitialSQLToProto(in *SchemaInitialSQL) *questionmanagerv1.SchemaInitialSQL
+
+	// goverter:ignore state sizeCache unknownFields
+	// goverter:map ID Id
+	// goverter:map SchemaID SchemaId
+	QuestionToProto(in *Question) *questionmanagerv1.Question
+
+	// goverter:enum:unknown Difficulty_DIFFICULTY_UNSPECIFIED
+	// goverter:enum:map DifficultyUnspecified Difficulty_DIFFICULTY_UNSPECIFIED
+	// goverter:enum:map DifficultyEasy Difficulty_DIFFICULTY_EASY
+	// goverter:enum:map DifficultyMedium Difficulty_DIFFICULTY_MEDIUM
+	// goverter:enum:map DifficultyHard Difficulty_DIFFICULTY_HARD
+	DifficultyToProto(in Difficulty) questionmanagerv1.Difficulty
 }
 
 func TimeToTimestamp(t time.Time) *timestamppb.Timestamp {
 	return timestamppb.New(t)
+}
+
+func UUIDToString(id uuid.UUID) string {
+	return id.String()
 }
