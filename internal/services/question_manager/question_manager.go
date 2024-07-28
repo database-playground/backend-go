9a47a1d07da96294d353c4d8e585e90e367f1e6b
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-29 00:45:16 +0800

    feat(question_manager/service): Implement protobuf interface

---
+++ internal/services/question_manager/question_manager.go
@@ -0,0 +1,25 @@
+package questionmanager
+
+import (
+	"github.com/database-playground/backend/gen/questionmanager/v1/questionmanagerv1connect"
+	"github.com/database-playground/backend/internal/database"
+	"github.com/database-playground/backend/internal/models"
+	"github.com/database-playground/backend/internal/models/generated"
+	"go.uber.org/fx"
+)
+
+var FxModule = fx.Module("question-manager-service", fx.Provide(New))
+
+type Service struct {
+	questionmanagerv1connect.UnimplementedQuestionManagerServiceHandler
+
+	db        *database.Database
+	converter models.Converter
+}
+
+func New(database *database.Database) *Service {
+	return &Service{
+		db:        database,
+		converter: &generated.ConverterImpl{},
+	}
+}
---
+++ internal/services/question_manager/schema_query.go
@@ -0,0 +1,44 @@
+package questionmanager
+
+import (
+	"context"
+	"errors"
+
+	"connectrpc.com/connect"
+	questionmanagerv1 "github.com/database-playground/backend/gen/questionmanager/v1"
+	"github.com/database-playground/backend/internal/database"
+)
+
+func (s *Service) GetSchema(ctx context.Context, request *connect.Request[questionmanagerv1.GetSchemaRequest]) (*connect.Response[questionmanagerv1.GetSchemaResponse], error) {
+	schema, err := s.db.GetSchema(ctx, request.Msg.GetId())
+	if errors.Is(err, database.ErrNotFound) {
+		return nil, connect.NewError(connect.CodeNotFound, err)
+	}
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	schemaPb := s.converter.SchemaToProto(schema)
+	return &connect.Response[questionmanagerv1.GetSchemaResponse]{
+		Msg: &questionmanagerv1.GetSchemaResponse{
+			Schema: schemaPb,
+		},
+	}, nil
+}
+
+func (s *Service) GetSchemaInitialSQL(ctx context.Context, request *connect.Request[questionmanagerv1.GetSchemaInitialSQLRequest]) (*connect.Response[questionmanagerv1.GetSchemaInitialSQLResponse], error) {
+	schemaInitialSQL, err := s.db.GetSchemaInitialSQL(ctx, request.Msg.GetId())
+	if errors.Is(err, database.ErrNotFound) {
+		return nil, connect.NewError(connect.CodeNotFound, err)
+	}
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	schemaInitialSQLPb := s.converter.SchemaInitialSQLToProto(schemaInitialSQL)
+	return &connect.Response[questionmanagerv1.GetSchemaInitialSQLResponse]{
+		Msg: &questionmanagerv1.GetSchemaInitialSQLResponse{
+			SchemaInitialSql: schemaInitialSQLPb,
+		},
+	}, nil
+}
