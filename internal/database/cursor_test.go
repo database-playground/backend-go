a43056c1f469e646f9da0450ee769421e764fb78
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-29 03:40:30 +0800

    test(database): Use EqualValues to compare int64 and int

--- internal/database/cursor_test.go
+++ internal/database/cursor_test.go
@@ -13,20 +13,20 @@ func TestCursor(t *testing.T) {
 	t.Run("limit 10, offset 0 by default", func(t *testing.T) {
 		cursor := database.Cursor{}
 
-		assert.Equal(t, 10, cursor.GetLimit())
-		assert.Equal(t, 0, cursor.GetOffset())
+		assert.EqualValues(t, 10, cursor.GetLimit())
+		assert.EqualValues(t, 0, cursor.GetOffset())
 	})
 
 	t.Run("limit 99, offset 99 is acceptable", func(t *testing.T) {
 		cursor := database.Cursor{Limit: 99, Offset: 99}
 
-		assert.Equal(t, 99, cursor.GetLimit())
-		assert.Equal(t, 99, cursor.GetOffset())
+		assert.EqualValues(t, 99, cursor.GetLimit())
+		assert.EqualValues(t, 99, cursor.GetOffset())
 	})
 
 	t.Run("limit over 100 should be rounded to 100", func(t *testing.T) {
 		cursor := database.Cursor{Limit: 114514}
 
-		assert.Equal(t, 100, cursor.GetLimit())
+		assert.EqualValues(t, 100, cursor.GetLimit())
 	})
 }
