3c65e5209845046dd41518d5f07abcbd8dc49e0f
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-29 03:03:34 +0800

    test(database): List questions with offset/limit

--- internal/database/question_query_test.go
+++ internal/database/question_query_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/database-playground/backend/internal/database"
+	"github.com/database-playground/backend/internal/models"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -19,14 +21,70 @@ func TestListQuestions(t *testing.T) {
 	require.NoError(t, db.Migrate(ctx))
 	require.NoError(t, db.SeedTestOnly(ctx))
 
-	questions, err := db.ListQuestions(ctx, database.ListQuestionsParams{})
+	rootQuestions, err := db.ListQuestions(ctx, database.ListQuestionsParams{})
 	if err != nil {
 		t.Fatalf("failed to get schema: %v", err)
 	}
 
-	assert.Len(t, questions, 10) // default offset=0, limit=10
-	assert.Equal(t, "Find a product in the shop", questions[0].Title)
-	t.Logf("%#v", questions)
+	assert.Len(t, rootQuestions, 10) // default offset=0, limit=10
+	assert.Equal(t, "Find a product in the shop", rootQuestions[0].Title)
+	t.Logf("%#v", rootQuestions)
+
+	t.Run("offset=1; limit=5", func(t *testing.T) {
+		questions, err := db.ListQuestions(ctx, database.ListQuestionsParams{Offset: 1, Limit: 5})
+		if err != nil {
+			t.Fatalf("failed to get schema: %v", err)
+		}
+
+		t.Logf("%#v", lo.Map(questions, func(q *models.Question, _ int) string {
+			return q.Title
+		}))
+
+		assert.Len(t, questions, 5)
+
+		for i, q := range questions {
+			assert.Equal(t, rootQuestions[i+1].Title, q.Title)
+		}
+	})
+
+	t.Run("offset=5; limit=5", func(t *testing.T) {
+		questions, err := db.ListQuestions(ctx, database.ListQuestionsParams{Offset: 5, Limit: 5})
+		if err != nil {
+			t.Fatalf("failed to get schema: %v", err)
+		}
+
+		assert.Len(t, questions, 5)
+
+		for i, q := range questions {
+			assert.Equal(t, rootQuestions[i+5].Title, q.Title)
+		}
+	})
+
+	t.Run("offset=0; limit=5", func(t *testing.T) {
+		questions, err := db.ListQuestions(ctx, database.ListQuestionsParams{Offset: 0, Limit: 5})
+		if err != nil {
+			t.Fatalf("failed to get schema: %v", err)
+		}
+
+		assert.Len(t, questions, 5)
+
+		for i, q := range questions {
+			assert.Equal(t, rootQuestions[i].Title, q.Title)
+		}
+	})
+
+	t.Run("limit=0 should be represented as limit=10", func(t *testing.T) {
+		questions, err := db.ListQuestions(ctx, database.ListQuestionsParams{Limit: 0})
+		if err != nil {
+			t.Fatalf("failed to get schema: %v", err)
+		}
+
+		assert.Len(t, questions, 10)
+
+		for i, q := range questions {
+			assert.Equal(t, rootQuestions[i].Title, q.Title)
+		}
+	})
 }
 
 func TestGetQuestion(t *testing.T) {
@@ -45,7 +103,7 @@ func TestGetQuestion(t *testing.T) {
 		t.Fatalf("failed to get schema: %v", err)
 	}
 
-	// Get a question
+	// Check if GetQuestion returns the same question
 	for _, listQuestion := range questions {
 		getQuestion, err := db.GetQuestion(ctx, listQuestion.ID)
 		if err != nil {
