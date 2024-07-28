3b7ea9880b2c635522d1f63fc48dc50ba8a4eed6
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-29 03:18:48 +0800

    feat(database): Add protobuf and converter of Cursor

--- internal/database/cursor.go
+++ internal/database/cursor.go
@@ -1,6 +1,9 @@
 package database
 
-import "github.com/samber/lo"
+import (
+	commonv1 "github.com/database-playground/backend/gen/common/v1"
+	"github.com/samber/lo"
+)
 
 type Cursor struct {
 	Offset int64
@@ -13,4 +16,15 @@ func (c Cursor) GetOffset() int64 {
 
 func (c Cursor) GetLimit() int64 {
 	return min(lo.CoalesceOrEmpty(c.Limit, 10), 100)
+}
+
+func CursorFromProto(cursor *commonv1.Cursor) Cursor {
+	if cursor == nil {
+		return Cursor{}
+	}
+
+	return Cursor{
+		Limit:  cursor.GetLimit(),
+		Offset: cursor.GetOffset(),
+	}
 }
