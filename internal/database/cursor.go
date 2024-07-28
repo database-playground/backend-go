1a13741adcddb7f649d17a3a281b79a0488a0f5f
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-29 03:18:19 +0800

    refactor(database): Cursor should be int64

--- internal/database/cursor.go
+++ internal/database/cursor.go
@@ -3,14 +3,14 @@ package database
 import "github.com/samber/lo"
 
 type Cursor struct {
-	Offset int
-	Limit  int
+	Offset int64
+	Limit  int64
 }
 
-func (c Cursor) GetOffset() int {
+func (c Cursor) GetOffset() int64 {
 	return c.Offset
 }
 
-func (c Cursor) GetLimit() int {
+func (c Cursor) GetLimit() int64 {
 	return min(lo.CoalesceOrEmpty(c.Limit, 10), 100)
 }
