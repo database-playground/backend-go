16da4ac5b266535f7f02242daf0ec9b79bb23317
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-29 22:53:50 +0800

    feat(devenv): devenv up starts microservices

--- devenv.nix
+++ devenv.nix
@@ -7,7 +7,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.buf pkgs.cfssl pkgs.sqlfluff ];
+  packages = [ pkgs.curl pkgs.git pkgs.buf pkgs.cfssl pkgs.sqlfluff ];
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";
@@ -36,7 +36,34 @@
   # pre-commit.hooks.shellcheck.enable = true;
 
   # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
+  processes.dbrunner-service = {
+    exec = "go run ./cmd/dbrunner-service";
+    process-compose = {
+      depends_on = {
+        redis.condition = "process_healthy";
+      };
+      readiness_probe = {
+        exec.command = "curl --cacert scripts/cert/ca-dev.pem --cert scripts/cert/client.pem --key scripts/cert/client-key.pem https://localhost:3000/healthz";
+      };
+      environment = [
+        "PORT=3000"
+      ];
+    };
+  };
+  processes.question-manager-service = {
+    exec = "go run ./cmd/question-manager-service";
+    process-compose = {
+      depends_on = {
+        postgres.condition = "process_healthy";
+      };
+      readiness_probe = {
+        exec.command = "curl --cacert scripts/cert/ca-dev.pem --cert scripts/cert/client.pem --key scripts/cert/client-key.pem https://localhost:3001/healthz";
+      };
+      environment = [
+        "PORT=3001"
+      ];
+    };
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
